eee1632b24d596a2da6632e5db32d79e1f4d9e77
Germán <barreragerman27@gmail.com>
2023-09-27 11:44:01 -0300

    TypeInference class was created as example

---
+++ src/main/kotlin/TypeInference.kt
@@ -0,0 +1,14 @@
+class TypeInference {
+}
+
+fun main(){
+    //we can not declare the type of the variable, kotlin knows it by default
+    var n = 10
+    var l = 150L
+    var d = 3.14
+    var f = 3.14F
+    var b= true
+    var s = "Yo Soy el Yo Soy"
+    var c = 'G'
+
+}
