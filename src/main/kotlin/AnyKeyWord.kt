2db33317d65ace78dbef26ca3c227d63f471123e
Germán <barreragerman27@gmail.com>
2023-09-27 11:38:07 -0300

    Any class was created as example

---
+++ src/main/kotlin/AnyKeyWord.kt
@@ -0,0 +1,9 @@
+class AnyKeyWord {
+}
+
+fun main(){
+    //Any key word allows us to set any type of datatype to a variable,
+    // but it is highly not recommended
+    var number: Any = "String"
+    var words: Any = 147
+}
