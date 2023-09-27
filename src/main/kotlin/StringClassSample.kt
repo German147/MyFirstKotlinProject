8d7543726e64dd441ba0210e4fa2abeec5757c6d
Germán <barreragerman27@gmail.com>
2023-09-27 12:36:50 -0300

    String class was created with some common examples

---
+++ src/main/kotlin/StringClassSample.kt
@@ -0,0 +1,15 @@
+class StringClassSample {
+}
+
+fun main(){
+
+    var name = "LuiGermanBarrera"
+    println(name.uppercase())
+    println(name.lowercase())
+    println(name.length)
+    println(name[0])
+    println(name[3])
+    println(name[9])
+    println(name.isEmpty())
+    println("".isEmpty())
+}
