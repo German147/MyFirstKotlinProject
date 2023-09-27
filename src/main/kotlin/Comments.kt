0b5d544c3a39d55804321f4e9e90e71fc0fc6448
Germán <barreragerman27@gmail.com>
2023-09-27 11:17:17 -0300

    Initial commit

---
+++ src/main/kotlin/Comments.kt
@@ -0,0 +1,6 @@
+class Comments {
+
+    fun main(){
+
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,8 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+    var name: String = "german"
+    var age: Int = 40
+    println(name + " is "+ age)
+    //the dollar $ provides you the ability to add info without the + sign
+    println("The name ${name} is ${age}" )
+}
