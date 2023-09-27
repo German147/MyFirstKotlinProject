29e74c915d3dc410471341608173aa14c80e8510
Germán <barreragerman27@gmail.com>
2023-09-27 11:31:13 -0300

    Definition of different data types class was created.

---
+++ src/main/kotlin/Datatype.kt
@@ -0,0 +1,10 @@
+fun main(){
+    var n: Int = 10
+    var l:Long = 150L
+    var d: Double = 3.14
+    var f:Float = 3.14F
+    var b:Boolean = true
+    var s: String = "Yo Soy el Yo Soy"
+    var c:Char = 'G'
+
+    }
