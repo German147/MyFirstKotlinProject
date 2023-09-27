97dc0124bb3cf887de6a384e35caf71f7cd61dcf
Germán <barreragerman27@gmail.com>
2023-09-27 14:28:38 -0300

    Multiline Strings and %s format expression was implemented

---
+++ src/main/kotlin/MultiLineStrings.kt
@@ -0,0 +1,20 @@
+class MultiLineStrings {
+}
+
+fun main(){
+
+    var name = "German"
+    var surname = "Barrera"
+
+    val msg = "My name %s is $name and my surname is $surname"
+    println(msg.format("an old name"))
+
+    val emai = """
+        Hi %s
+        this 
+        is  my 
+        Multiline Strings
+    """.trimIndent()
+
+    println(emai.format("Little DGerman"))
+}
