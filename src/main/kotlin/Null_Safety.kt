540b9bcd09b26b926d0aace584cba8d8c38b208f
Germán <barreragerman27@gmail.com>
2023-09-27 12:26:43 -0300

    Creating a null variable was created

---
+++ src/main/kotlin/Null_Safety.kt
@@ -0,0 +1,12 @@
+class Null_Safety {
+}
+fun main(){
+
+    var name = "Jamila"
+    println(name.uppercase())
+        //Here we can set a null value to a variable only using the "?"
+        // at the end of it and the type of.
+    var brand : String? = null
+    println(brand?.uppercase())
+
+}
