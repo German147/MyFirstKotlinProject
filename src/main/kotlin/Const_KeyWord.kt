3736fa45c216a585d7b65a0905406ac06584a378
Germán <barreragerman27@gmail.com>
2023-09-27 12:17:09 -0300

    Const val keyword class was created as example

---
+++ src/main/kotlin/Const_KeyWord.kt
@@ -0,0 +1,12 @@
+class Const_KeyWord {
+}
+//it is like Static final in java usint the const val expression
+const val ger = "german1"
+fun main(){
+
+    //it is not possible because it is like Static final in java
+        println(ger)
+       // ger = "luis"
+
+
+}
