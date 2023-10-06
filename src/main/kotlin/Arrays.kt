b8f4827c31a8574a43ea06da0324c92d9c2fc5cd
Germán <barreragerman27@gmail.com>
2023-10-06 10:58:14 -0300

    Here it was created the arrays class for practice.

---
+++ src/main/kotlin/Arrays.kt
@@ -0,0 +1,13 @@
+class Arrays {
+}
+fun main(){
+    val names = arrayOf("evange","marti","Lean","german")
+
+    println(names[0])
+    println(names[1])
+    println(names[2])
+    println(names[3])
+    println(names.contentToString())
+    println("The number of names is " + names.size + " into this array")
+    println("here is the sorted array "+ names.sortedArrayDescending().contentToString())
+}
