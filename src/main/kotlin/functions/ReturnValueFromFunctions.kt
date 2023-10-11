8d836aa25f44bae94917af216c6cfaa8622fb3a4
Germán <barreragerman27@gmail.com>
2023-10-11 15:53:39 -0300

    It was added some functionalities on single Functions sample on ReturnVal

--- src/main/kotlin/functions/ReturnValueFromFunctions.kt
+++ src/main/kotlin/functions/ReturnValueFromFunctions.kt
@@ -9,6 +9,12 @@ fun main(){
 
     val word = changeToUpperCase("samsung")
     println(word)
+
+     val res = singleFunc(3)
+    println(res)
+
+    val res2 = avoidReturnType(4.2)
+    println(res2)
 }
 
 fun sumInt(number: Int):Int {
@@ -19,5 +25,11 @@ fun changeToUpperCase(word: String):String{
     var result = word.uppercase()
     return result
 }
+
+//Using single function expression we don't need the curly brace
+fun singleFunc(num:Int):Int = num * 2
+//we can also avoid the return type in single function expression
+fun avoidReturnType(num:Double) = num + 4
+
 
 
