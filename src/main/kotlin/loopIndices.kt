b3ef5e51f28e8a67e8d6f4166090cbb4b1655594
Germán <barreragerman27@gmail.com>
2023-10-11 08:25:16 -0300

    Here it was created the indices  class for practice.

---
+++ src/main/kotlin/loopIndices.kt
@@ -0,0 +1,14 @@
+class loopIndices {
+}
+fun main(){
+    //Usinig any type of array we can implement 'indices'
+   val letters = charArrayOf('A','B','C','D','E')
+    for (index in letters.indices){
+        println("$index - ${letters[index]}")
+    }
+    println("Now Lets make it reverse")
+    for (index in letters.indices.reversed()){
+        println("$index - ${letters[index]}")
+    }
+
+}
