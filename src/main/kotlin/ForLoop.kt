a25f244b3d69a7139fc401fcf004731a4f094cc7
Germán <barreragerman27@gmail.com>
2023-10-10 22:23:49 -0300

    Here it was created the forLoop  class for practice.

---
+++ src/main/kotlin/ForLoop.kt
@@ -0,0 +1,17 @@
+class ForLoop {
+}
+fun main(){
+    val names =listOf("luna","gaturro","tom","Jerry")
+    val numbers =intArrayOf(1,2,3,4,5,6,7,8,9,10)
+
+    for (name in names){
+        val n = name.replaceFirstChar { it.uppercase() }
+        println(n)
+    }
+
+
+    println("------------------------")
+    for (num in numbers){
+        println(num)
+    }
+}
