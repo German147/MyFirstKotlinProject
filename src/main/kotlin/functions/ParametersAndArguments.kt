2af7123bbd93ebd352da1fe08fc647b8693730e2
Germán <barreragerman27@gmail.com>
2023-10-11 11:35:29 -0300

    Here it was added an explanation about explicity named arguments

--- src/main/kotlin/functions/ParametersAndArguments.kt
+++ src/main/kotlin/functions/ParametersAndArguments.kt
@@ -1,10 +1,14 @@
 package functions
 
 fun main(){
+    //We can also explicity say the arguments we want, and also change the order
+    greet2(age=41,name="Laura")
+    greet2(name="Lucy",age=10)
    greet2("German",40)
    greet2("Marti",7)
    greet2("Lean",10)
    greet2("Evange",43)
+
 }
 
 fun greet2(name: String, age: Int) {
